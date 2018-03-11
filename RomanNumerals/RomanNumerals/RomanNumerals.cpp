55f4532babcabf47d5ab3a0c17423678044e7675
Navjot Kamal <navjotcs@Gmail.com>
2018-03-11 04:25:58 -0700

    Red

--- RomanNumerals/RomanNumerals/RomanNumerals.cpp
+++ RomanNumerals/RomanNumerals/RomanNumerals.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    // Test Code 1
+    string input = "I";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "V";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "X";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "L";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "C";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "D";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+    input = "M";
+    std::cout << "Decimal numeral of " << input << " is " << romanNumeralCharToDecimal(input) << std::endl;
+
+    cin.get();
+
+    return 0;
 }
 
